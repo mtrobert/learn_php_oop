fc54a9cb780001cd77a1bd67925adaa34235c961
Robert Marczak <robert.m.1305@gmail.com>
2018-04-09 12:19:45 +0100

    First commit in the Learn php oop course. Includes creating object and methods and properties with in it.

---
+++ task.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *
+ */
+class Task
+{
+  public $seriesTitle, $name;
+  public $completed = false;
+
+  public function __construct($seriesTitle, $name)
+  {
+    $this->seriesTitle = $seriesTitle;
+    $this->name = $name;
+
+  }
+
+  public function complete()
+  {
+    $this->completed = true;
+  }
+
+}
+
+$task = new Task('Learning OOP','Robert Marczak');
+
+var_dump($task->seriesTitle);
+
+var_dump($task->name);
+
+$task->complete();
+
+var_dump($task->completed);
+
+ ?>
