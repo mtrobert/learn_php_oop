29b6807edae25cb7963b2f688a6e2dbf8d2d6fd7
Robert Marczak <robert.m.1305@gmail.com>
2018-04-12 12:54:19 +0100

    Added two more properties plus getters and setter to this properties to the class Taxi.

--- Taxi.php
+++ Taxi.php
@@ -1,6 +1,54 @@
 <?php
 
 include ('Car.php');
+/**
+ * represents taxis
+ */
+class Taxi extends Car
+{
+  private $numberOfPassengers;
+  private $passengerOn;         //identyfies if the taxi is free
+
+  public function accelerate()
+  {
+    $this->accelerate = 'slow';
+
+  }
+
+
+  public function setNumberOfPassengers($numberOfPassengers)
+  {
+    $this->numberOfPassengers = $numberOfPassengers;
+  }
+
+  public function getNumberOfPassengers()
+  {
+    return $this->numberOfPassengers;
+  }
+
+  public function setPassengersOn($passengerOn)
+  {
+   $this->passengerOn =  $passengerOn;
+  }
+
+  public function gerPassengersOn()
+  {
+    return $this->passengersOn;
+  }
+
+
+}
+
+
+$taxi = new Taxi();
+$taxi->setColour('black');
+$taxi->setDoorNumber(4);
+$taxi->setFuelType('Diesel');
+$taxi->setNumberOfPassengers(2);
+$taxi->setPassengersOn(true);
+$taxi->accelerate();
+echo "\n";
+var_dump($taxi);
 
 
 ?>
