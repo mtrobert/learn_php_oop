5900fec55d43a4cdee5702ce2a0e505a775fe2e7
Robert Marczak <robert.m.1305@gmail.com>
2018-04-12 12:18:30 +0100

    Implemented an abstract method in a subclass.

--- Sport_car.php
+++ Sport_car.php
@@ -1,11 +1,26 @@
 <?php
+
+include ('Car.php');
 /**
  * represents sport cars
  */
 class Sport_car extends Car
 {
-  
+
+  public function accelerate()
+  {
+    $this->accelerate = 'fast';
+  }
+
 }
+
+$sport_car = new Sport_car();
+$sport_car->setColour('red');
+$sport_car->setDoorNumber(2);
+$sport_car->setFuelType('Petrol');
+$sport_car->accelerate();
+echo "\n";
+var_dump($sport_car);
 
 
 
