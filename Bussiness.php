b069f5bb40e78af5d19eaee067e5609e216e783d
Robert Marczak <robert.m.1305@gmail.com>
2018-04-15 19:01:18 +0100

    Added three classes plus methods and properties within those classes.

--- Bussiness.php
+++ Bussiness.php
@@ -1,15 +1,30 @@
 <?php
 
 /**
- *
+ *A file with simple example of 'messaging' concept in OOP.
  */
+
+
+
 class Bussiness
 {
 
-  function __construct(argument)
+  protected $staff;
+
+  function __construct(Staff $staff)
   {
-    # code...
+    $this->staff = $staff;
   }
+  public function hire(Person $person)
+  {
+    $this->staff->add($person);           //sending a message to the Staff class that we want to add person to members
+  }
+
+  public function getStaffMembers()
+  {
+    return $this->staff->members();       //sending a message to the Staff class that we want the memmbers
+  }
+
 }
 
 /**
@@ -18,10 +33,23 @@ class Bussiness
 class Staff
 {
 
-  function __construct(argument)
+  protected $members = [];
+
+  public function __construct($members =[])
   {
-    # code...
+    $this->members = $members;
   }
+
+  public function add(Person $person)
+  {
+    $this->members[] = $person;
+  }
+
+  public function members()
+  {
+    return $this->members;
+  }
+
 }
 
 /**
@@ -30,15 +58,21 @@ class Staff
 class Person
 {
 
-  function __construct(argument)
+  public $name, $profession ;
+
+  function __construct($name = 'Robert', $profession = 'Butcher')
   {
-    # code...
+    $this->name = $name;
+    $this->profession = $profession;
   }
 }
 
-
-
-
+$person = new Person();
+$staff = new Staff([$person]);
+$b_and_b = new Bussiness($staff);
+$person2 = new Person('Jonathan', 'Baker');
+$b_and_b->hire($person2);
+var_dump($b_and_b->getStaffMembers());
 
 
 ?>
