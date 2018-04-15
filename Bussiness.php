e9abbe851be1318e0970cf7d846b8208d5b933e6
Robert Marczak <robert.m.1305@gmail.com>
2018-04-15 17:14:44 +0100

    Created Bussines.php file. A simple example of 'messaging' concept in OOP.

---
+++ Bussiness.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ *
+ */
+class Bussiness
+{
+
+  function __construct(argument)
+  {
+    # code...
+  }
+}
+
+/**
+ *
+ */
+class Staff
+{
+
+  function __construct(argument)
+  {
+    # code...
+  }
+}
+
+/**
+ *
+ */
+class Person
+{
+
+  function __construct(argument)
+  {
+    # code...
+  }
+}
+
+
+
+
+
+
+?>
