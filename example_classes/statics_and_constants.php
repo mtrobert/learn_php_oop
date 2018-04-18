7abaf1b30bb84ff65593bf8759db12910d1de7e9
Robert Marczak <robert.m.1305@gmail.com>
2018-04-18 13:21:11 +0100

    Added StrAction class with static methods to the file statics_and_constants.php

--- example_classes/statics_and_constants.php
+++ example_classes/statics_and_constants.php
@@ -9,20 +9,63 @@ class Words
 
   public  function addWords(...$string)
   {
-    $this->words[] = $string;
+    $this->words = array_merge($this->words, $string);
   }
+
+  public function getWords()
+  {
+    return $this->words;
+  }
+
+}
+
+class StrAction
+{
+
+  public static function lowerCase($str)
+  {
+    if (is_array($str)) {
+
+      $array = [];
+
+      foreach ($str as $word) {
+
+        $newWord = strtolower($word) . " ->lowercased from original " . $word;
+        $array[] = $newWord;
+      }
+
+      return $array ;
+    }else{
+      return strtolower($str);
+    }
+  }
+
+  public static function upperCase($str)
+  {
+    if (is_array($str)) {
+
+      $array = [];
+
+      foreach ($str as $word) {
+
+        $newWord = strtoupper($word) . " ->uppercased from original " . $word;
+        $array[] = $newWord;
+      }
+
+      return $array ;
+    }else{
+      return strtoupper($str);
+    }
+  }
+
 }
 
 $wordBank = new Words();
-$wordBank->addWords('siemanko', 'siema', 'siemaneczko');
-print_r($wordBank->words);
+$wordBank->addWords('HELLO', 'Good Morning', 'alRight Mate');
+var_dump($wordBank->words);
 
-
-
-
-
-
-
+print_r(StrAction::upperCase($wordBank->getWords()));
+print_r(StrAction::lowerCase($wordBank->getWords()));
 
 
 
