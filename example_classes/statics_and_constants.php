97a5bea82277feba10faec644431bc2953ff6636
Robert Marczak <robert.m.1305@gmail.com>
2018-04-18 11:37:49 +0100

    Created example class Words that adds words to the array.

--- example_classes/statics_and_constants.php
+++ example_classes/statics_and_constants.php
@@ -1,7 +1,29 @@
 <?php
+/**
+ * simple examples with statics and constants
+ */
+class Words
+{
+
+  public $words = [];
+
+  public  function addWords(...$string)
+  {
+    $this->words[] = $string;
+  }
+}
+
+$wordBank = new Words();
+$wordBank->addWords('siemanko', 'siema', 'siemaneczko');
+print_r($wordBank->words);
 
 
 
 
 
- ?>
+
+
+
+
+
+?>
