4612bf546bf439adf049121a5e93026f59e6600e
Robert Marczak <robert.m.1305@gmail.com>
2018-04-19 13:07:52 +0100

    Created two classes in statics_and_constants.php for simple constant example.

--- example_classes/statics_and_constants.php
+++ example_classes/statics_and_constants.php
@@ -2,6 +2,7 @@
 /**
  * simple examples with statics and constants
  */
+
 class Words
 {
 
@@ -65,7 +66,42 @@ $wordBank->addWords('HELLO', 'Good Morning', 'alRight Mate');
 var_dump($wordBank->words);
 
 print_r(StrAction::upperCase($wordBank->getWords()));
+
 print_r(StrAction::lowerCase($wordBank->getWords()));
+
+
+class Life
+{
+  protected $human, $statement;
+  const MUST_DO = ['PaY tAx', 'DIe'];         //constants never change :)
+
+  function __construct(Human $human)
+  {
+    $this->human = $human;
+    $this->statement = $this->human->name . ' be sure of two things in your life: you have to ' . StrAction::lowerCase(Life::MUST_DO[0]) . ' and you have to '. StrAction::lowerCase(Life::MUST_DO[1] . '.');
+  }
+
+  public function getStatement()
+  {
+    return $this->statement;
+  }
+
+}
+ class Human extends Life
+ {
+   protected $name;
+
+   public function __construct($name)
+   {
+     $this->name = $name;
+   }
+
+ }
+ $human = new Human('Robert');
+ $life = new Life($human);
+
+ echo "\n" . $life->getStatement();
+
 
 
 
