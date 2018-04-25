5b8bb4cc111cddf252f89dec17ec91108ef091cc
Robert Marczak <robert.m.1305@gmail.com>
2018-04-25 12:54:01 +0100

    Added second argument to execute() method in RegisterUser.php

--- src/RegisterUser.php
+++ src/RegisterUser.php
@@ -7,8 +7,12 @@ namespace b_and_b;
 class RegisterUser
 {
 
+  public function execute(array $userData, $listener)         //user data could be an object or an array
+  {                                                           //$listener reffers to the object it is called in ($this as an argument)
+    var_dump('Registering user using the data provided.');
 
 
+  }
 
 }
 
