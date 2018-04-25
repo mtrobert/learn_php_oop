5f8c7c5d1a180fd2695f23899a280eedd4a56c4f
Robert Marczak <robert.m.1305@gmail.com>
2018-04-25 11:38:11 +0100

    Created two methods to respond on the registration status in AuthController.php

--- src/AuthController.php
+++ src/AuthController.php
@@ -6,9 +6,26 @@ namespace b_and_b;
 class AuthController
 {
 
+  protected $registration;
+
+  public function __construct(RegisterUser $registration)
+  {
+    $this->registration = $registration;
+  }
+
   public function register()
   {
+    $this->registration->execute();
+  }
 
+  public function userRegisteredSuccessfully()
+  {
+    var_dump('User registered  successfully.');
+  }
+
+  public function userRegistrationFailed()
+  {
+    var_dump('User registration failed.');
   }
 
 
