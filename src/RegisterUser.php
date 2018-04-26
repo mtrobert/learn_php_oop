d1d4386c1316ac0a3756d1540ab46a1ea20f2b65
Robert Marczak <robert.m.1305@gmail.com>
2018-04-26 10:14:27 +0100

    Type hinted second argument for execute() in RegisterUser.php

--- src/RegisterUser.php
+++ src/RegisterUser.php
@@ -7,7 +7,7 @@ namespace b_and_b;
 class RegisterUser
 {
 
-  public function execute(array $userData, $listener)         //user data could be an object or an array
+  public function execute(array $userData, interfaces\RespondsToUserRegistration $listener)         //user data could be an object or an array
   {                                                           //$listener reffers to the object it is called in ($this as an argument)
     var_dump('Registering user using the data provided.');
 
